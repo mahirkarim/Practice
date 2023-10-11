9a0d0a6ed1230073e524901727b401bca2f2bf0e
mahirkarim <mahirkarim@utexas.edu>
2023-10-10 19:27:27 -0500

    improved zigzag

--- Problems/LeetCode Top Interview Questions/6 Zigzag Conversion.py
+++ Problems/LeetCode Top Interview Questions/6 Zigzag Conversion.py
@@ -1,5 +1,45 @@
 class Solution(object):
     # slow and memory intensive, but passes
+    # def convert(self, s, numRows):
+    #     """
+    #     :type s: str
+    #     :type numRows: int
+    #     :rtype: str
+    #     """
+
+    #     if numRows > len(s):
+    #         return s
+    #     if numRows == 1:
+    #         return s
+    #     x = 0
+    #     y = 0
+    #     ascending = 0
+    #     matrix_dict = dict()
+    #     for c in s:
+    #         if ascending:
+    #             x += 1
+    #             y -= 1
+    #             for i in range(numRows):
+    #                 matrix_dict[x,i] = ''
+    #             matrix_dict[x, y] = c
+    #         else:
+    #             matrix_dict[x, y] = c
+    #             y += 1
+    #         if y == 0:
+    #             ascending = 0
+    #             y += 1
+    #         elif y == numRows:
+    #             ascending = 1
+    #             y -= 1
+        
+    #     return_string = ''
+
+    #     for i in range(numRows):
+    #         for j in range(0, x+1):
+    #             return_string += matrix_dict[j, i]
+    #     return return_string
+
+    # much better, but still pretty slow
     def convert(self, s, numRows):
         """
         :type s: str
@@ -11,30 +51,30 @@ class Solution(object):
             return s
         if numRows == 1:
             return s
-        x = 0
-        y = 0
+
+        rows_dict = dict()
+        for row in range(numRows):
+            rows_dict[row] = []
+
+        row = 0
         ascending = 0
-        matrix_dict = dict()
+
         for c in s:
             if ascending:
-                x += 1
-                y -= 1
-                for i in range(numRows):
-                    matrix_dict[x,i] = ''
-                matrix_dict[x, y] = c
+                row -= 1 
+                rows_dict[row].append(c)
             else:
-                matrix_dict[x, y] = c
-                y += 1
-            if y == 0:
+                rows_dict[row].append(c)
+                row += 1
+            if row == 0:
                 ascending = 0
-                y += 1
-            elif y == numRows:
+                row += 1
+            elif row == numRows:
                 ascending = 1
-                y -= 1
+                row -= 1
         
         return_string = ''
 
-        for i in range(numRows):
-            for j in range(0, x+1):
-                return_string += matrix_dict[j, i]
+        for row in range(numRows):
+            return_string += ''.join(rows_dict[row])
         return return_string
